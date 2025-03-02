12ec91163fed41cedc5534ac476a99a6adcbcc8d
Alice-xyz <2496546398@qq.com>
2025-03-02 14:49:23 +0800

    Update

---
+++ assets/js/custom.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let navTitle = document.querySelector(".site-title");
+    if (navTitle) {
+        navTitle.innerText = "Xiaoyan's Homepage";
+    }
+});
